dffb0ecdea5dbe867495cf5a55890ee396eb2042
Chris Johnson <christopherjohnson1@gmail.com>
2020-07-27 17:15:27 -0400

    wip

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { criminalList } from './criminals/criminalList.js'
-import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
+import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 criminalList()
 ConvictionSelect()
+OfficerSelect()
